1e147204bcb2e57aa13cfd606e40ea8453f71e4f
Bambo0st <99262296+Bambo0st@users.noreply.github.com>
2024-09-23 14:20:13 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter .
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
