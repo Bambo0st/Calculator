71873567ca528d3d9b4a3b8ccf7843397bf964ac
Bambo0st <Adithya.Somasalle@iiitb.ac.in>
2024-09-23 16:36:07 +0530

    Added TestCases and Calculator Code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,144 @@
 package org.example;
+
+import java.util.Scanner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    private static final Logger logger = LogManager.getLogger(Main.class);
 
-    public int add(int a , int b) {
-        return a + b;
+public class Main {
+
+    private static Logger logger;
+
+    public double findSqrt(double num) {
+        if (num < 0) {
+            logger.warn("Invalid input for square root: {}", num);
+            return -1;
+        }
+
+        double ans = Math.sqrt(num);
+        logger.info("[OPERATION] Square Root, [INPUT] {}, [RESULT] {}", num, ans);
+        return ans;
     }
-    public int sub(int a , int b)
-    {
-        return a-b;
+
+    public double findFact(double num) {
+        if (num < 0) {
+            logger.warn("Invalid input for factorial: {}", num);
+            return -1;
+        }
+
+        double ans = 1;
+        for (int i = 1; i <= num; i++) {
+            ans *= i;
+        }
+
+        logger.info("[OPERATION] Factorial, [INPUT] {}, [RESULT] {}", num, ans);
+        return ans;
+    }
+
+    public double findLog(double num) {
+        if (num <= 0) {
+            logger.warn("Invalid input for natural logarithm: {}", num);
+            return -1;
+        }
+
+        double ans = Math.log(num);
+        logger.info("[OPERATION] Natural Logarithm, [INPUT] {}, [RESULT] {}", num, ans);
+        return ans;
+    }
+
+    public double findPow(double num1, double num2) {
+        double ans = Math.pow(num1, num2);
+        logger.info("[OPERATION] Power, [INPUT1] {}, [INPUT2] {}, [RESULT] {}", num1, num2, ans);
+        return ans;
+    }
+
+    public Main() {
+        logger = LogManager.getLogger(Main.class);
+    }
+
+    public static void menu() {
+        System.out.println("Welcome to Scientific Calculator");
+        System.out.println("--------------------------");
+        System.out.println("1. Square root");
+        System.out.println("2. Factorial");
+        System.out.println("3. Natural logarithm (base e)");
+        System.out.println("4. Power");
+        System.out.println("0. Exit");
+        System.out.print("Enter your choice: ");
     }
 
     public static void main(String[] args) {
+        Main cal = new Main();
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
 
-        System.out.print("Hello and welcome!");
+        int choice;
 
+        do {
+            Main.menu();
+            choice = sc.nextInt();
+
+            double num1;
+            double num2;
+            double ans;
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter number: ");
+                    num1 = sc.nextDouble();
+                    ans = cal.findSqrt(num1);
+                    if (ans == -1) {
+                        System.out.println("Invalid input for square root entered.");
+                    } else {
+                        System.out.println("Square root of " + num1 + " is " + ans);
+                    }
+                    break;
+
+                case 2:
+                    System.out.print("Enter number: ");
+                    num1 = sc.nextDouble();
+                    ans = cal.findFact(num1);
+                    if (ans == -1) {
+                        System.out.println("Invalid input for factorial.");
+                    } else {
+                        System.out.println("Factorial of " + num1 + " is " + ans);
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Enter number: ");
+                    num1 = sc.nextDouble();
+                    ans = cal.findLog(num1);
+                    if (ans == -1) {
+                        System.out.println("Invalid input for natural logarithm.");
+                    } else {
+                        System.out.println("Natural logarithm (base e) of " + num1 + " is " + ans);
+                    }
+                    break;
+
+                case 4:
+                    System.out.print("Enter first number: ");
+                    num1 = sc.nextDouble();
+
+                    System.out.print("Enter second number: ");
+                    num2 = sc.nextDouble();
+
+                    ans = cal.findPow(num1, num2);
+                    System.out.println("Power of " + num1 + " to " + num2 + " is " + ans);
+                    break;
+
+                case 0:
+                    System.out.println("Exiting Calculator...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice entered. Please try again.");
+            }
+
+            System.out.println();
+        } while (choice != 0);
+
+        sc.close();
     }
+
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,9 +1,11 @@
 import org.example.Main;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class CalculatorTest {
+
+    private static final double DELTA = 1e-15;
     private Main calculator;
 
     @Before
@@ -12,35 +14,67 @@ public class CalculatorTest {
     }
 
     @Test
-    public void test_add_positive() {
-        int a = 1;
-        int b = 2;
-        int expectedResult = 3;
-        Assert.assertEquals(expectedResult, calculator.add(a, b));
+    public void testSquareRootValidInputs() {
+        assertEquals("Square Root Test 1", 2, calculator.findSqrt(4), DELTA);
+        assertEquals("Square Root Test 2", 3, calculator.findSqrt(9), DELTA);
+        assertEquals("Square Root Test 3", 4, calculator.findSqrt(16), DELTA);
+        assertEquals("Square Root Test 4", 1.4142135623730951, calculator.findSqrt(2), DELTA);
     }
 
     @Test
-    public void test_add_negative() {
-        int a = 1;
-        int b = 2;
-        int expectedResult = 0;
-        Assert.assertNotEquals(expectedResult, calculator.add(a, b));
+    public void testSquareRootInvalidInputs() {
+        assertNotEquals("Square Root Test 5", 3, calculator.findSqrt(4), DELTA);
+        assertNotEquals("Square Root Test 6", 2, calculator.findSqrt(9), DELTA);
+        assertNotEquals("Square Root Test 7", 2, calculator.findSqrt(15), DELTA);
+        assertNotEquals("Square Root Test 8", 3, calculator.findSqrt(-9), DELTA);
     }
 
     @Test
-    public void test_sub_positive() {
-        int a = 2;
-        int b = 2;
-        int expectedResult = 0;
-        Assert.assertEquals(expectedResult, calculator.sub(a, b));
+    public void testFactorialValidInputs() {
+        assertEquals("Factorial Test 1", 1, calculator.findFact(0), DELTA);
+        assertEquals("Factorial Test 2", 1, calculator.findFact(1), DELTA);
+        assertEquals("Factorial Test 3", 24, calculator.findFact(4), DELTA);
+        assertEquals("Factorial Test 4", 120, calculator.findFact(5), DELTA);
     }
 
     @Test
-    public void test_sub_negative() {
-        int a = 2;
-        int b = 2;
-        int expectedResult=3;
-        Assert.assertNotEquals(expectedResult, calculator.sub(a, b)) ;
+    public void testFactorialInvalidInputs() {
+        assertNotEquals("Factorial Test 5", 0, calculator.findFact(2), DELTA);
+        assertNotEquals("Factorial Test 6", 5, calculator.findFact(3), DELTA);
+        assertNotEquals("Factorial Test 7", 20, calculator.findFact(4), DELTA);
+        assertNotEquals("Factorial Test 8", 100, calculator.findFact(5), DELTA);
     }
 
+    @Test
+    public void testLogarithmValidInputs() {
+        assertEquals("Logarithm Test 1", 2.302585092994046, calculator.findLog(10), DELTA);
+        assertEquals("Logarithm Test 2", 4.605170185988092, calculator.findLog(100), DELTA);
+        assertEquals("Logarithm Test 3", 0, calculator.findLog(1), DELTA);
+
+    }
+
+    @Test
+    public void testLogarithmInvalidInputs() {
+        assertNotEquals("Logarithm Test 4", 3, calculator.findLog(10), DELTA);
+        assertNotEquals("Logarithm Test 5", 4, calculator.findLog(100), DELTA);
+        assertNotEquals("Logarithm Test 6", 1, calculator.findLog(1), DELTA);
+        assertNotEquals("Logarithm Test 7", 2, calculator.findLog(-100), DELTA);
+
+    }
+
+    @Test
+    public void testPowerValidInputs() {
+        assertEquals("Power Test 1", 4, calculator.findPow(2, 2), DELTA);
+        assertEquals("Power Test 2", 25, calculator.findPow(5, 2), DELTA);
+        assertEquals("Power Test 3", 3, calculator.findPow(3, 1), DELTA);
+        assertEquals("Power Test 4", 1, calculator.findPow(10, 0), DELTA);
+    }
+
+    @Test
+    public void testPowerInvalidInputs() {
+        assertNotEquals("Power Test 5", 5, calculator.findPow(2, 2), DELTA);
+        assertNotEquals("Power Test 6", 100, calculator.findPow(5, 3), DELTA);
+        assertNotEquals("Power Test 7", 0, calculator.findPow(3, 1), DELTA);
+        assertNotEquals("Power Test 8", 10, calculator.findPow(10, 0), DELTA);
+    }
 }
